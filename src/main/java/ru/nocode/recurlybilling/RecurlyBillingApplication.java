462e65995cd2e9b0d2ad6f20e6f9d5c486989561
matvey_elovskikh <mr1504dj@mail.ru>
2026-02-25 18:03:29 +0500

    init

---
+++ src/main/java/ru/nocode/recurlybilling/RecurlyBillingApplication.java
@@ -0,0 +1,13 @@
+package ru.nocode.recurlybilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecurlyBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecurlyBillingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nocode/recurlybilling/RecurlyBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nocode.recurlybilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecurlyBillingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
